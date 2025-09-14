f96cba21dd43f97599a8518348b5e56907114683
Gopinath <gopinath0332@outlook.com>
2025-09-14 13:01:01 +0530

    adding git actions

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/MyTradeJournal/', // Replace with your repository name
   build: {
-    target: 'es2015'
+    target: 'es2015',
+    outDir: 'dist'
   },
   esbuild: {
     target: 'es2015'
